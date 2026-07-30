b251ae0aadc1280ac82456c6fd49e941cc6a36df
rafi.fusi <rafi.fusi@plnindonesiapower.co.id>
2026-07-30 07:19:55 +0700

    Fix : fixing access limitation

--- resources/views/kebutuhan/index.blade.php
+++ resources/views/kebutuhan/index.blade.php
@@ -4,12 +4,14 @@
         <h4 class="fw-bold py-3"><span class="text-muted fw-light">Beranda /</span> Daftar Kebutuhan</h4>
         <div class="card shadow-sm border-0 w-100">
             <div class="card-body">
-                <div class="row pt-2 pb-4 align-items-end">
-                    <div class="col-lg-3">
-                        <a href="{{ route('kebutuhan.create') }}" class="btn btn-primary ms-auto"><i
-                                class="fa fa-plus me-2"></i>Tambah</a>
+                @if ($access['can_create'])
+                    <div class="row pt-2 pb-4 align-items-end">
+                        <div class="col-lg-3">
+                            <a href="{{ route('kebutuhan.create') }}" class="btn btn-primary ms-auto"><i
+                                    class="fa fa-plus me-2"></i>Tambah</a>
+                        </div>
                     </div>
-                </div>
+                @endif
                 <div class="table-responsive">
                     <table class="table table-bordered" id="table-kebutuhan">
                         <thead>
@@ -24,7 +26,9 @@
                                 <th>Jumlah Diterima</th>
                                 <th>Diperbarui Oleh</th>
                                 <th>Tanggal dan Waktu Diperbarui</th>
-                                <th width="10%">Aksi</th>
+                                @if ($access['can_edit'] && $access['can_create'])
+                                    <th width="10%">Aksi</th>
+                                @endif
                             </tr>
                         </thead>
                     </table>
@@ -33,58 +37,59 @@
         </div> <!-- /.card -->
     </div>
     @push('javascript')
-        <script>
-            $('#table-kebutuhan').DataTable({
-                responsive: true,
-                processing: true,
-                serverSide: true,
-                ajax: {
-                    url: "{{ route('kebutuhan.index') }}",
-                    error: function(xhr, error, code) {
-                        errorAlert(xhr.statusText);
-                    }
-                },
-                columns: [{
-                        data: null,
-                        render: function(data, type, row, meta) {
-                            return meta.settings._iDisplayStart + meta.row + 1;
+        @if ($access['can_edit'] && $access['can_create'])
+            <script>
+                $('#table-kebutuhan').DataTable({
+                    responsive: true,
+                    processing: true,
+                    serverSide: true,
+                    ajax: {
+                        url: "{{ route('kebutuhan.index') }}",
+                        error: function(xhr, error, code) {
+                            errorAlert(xhr.statusText);
                         }
                     },
-                    {
-                        data: 'posko.user.name'
-                    },
-                    {
-                        data: 'JudulKebutuhan'
-                    },
-                     {
-                        data: 'TanggalKebutuhan'
-                    },
-                    {
-                        data: 'barang.NamaBarang'
-                    },
-                    {
-                        data: 'barang.jenis_barang.JenisBarang'
-                    },
-                    {
-                        data: 'JumlahKebutuhan'
-                    },
-                    {
-                        data: 'JumlahDiterima'
-                    },
-                    {
-                        data: 'user.name'
-                    },
-                    {
-                        data: 'LastUpdateDate'
-                    },
-                    {
-                        data: 'IDKebutuhan',
-                        orderable: false,
-                        searchable: false,
-                        className: 'dt-center',
-                        render: function(data, type, row) {
-                            let baseUrl = `{{ url('kebutuhan') }}`;
-                            return `
+                    columns: [{
+                            data: null,
+                            render: function(data, type, row, meta) {
+                                return meta.settings._iDisplayStart + meta.row + 1;
+                            }
+                        },
+                        {
+                            data: 'posko.user.name'
+                        },
+                        {
+                            data: 'JudulKebutuhan'
+                        },
+                        {
+                            data: 'TanggalKebutuhan'
+                        },
+                        {
+                            data: 'barang.NamaBarang'
+                        },
+                        {
+                            data: 'barang.jenis_barang.JenisBarang'
+                        },
+                        {
+                            data: 'JumlahKebutuhan'
+                        },
+                        {
+                            data: 'JumlahDiterima'
+                        },
+                        {
+                            data: 'user.name'
+                        },
+                        {
+                            data: 'LastUpdateDate'
+                        },
+                        {
+                            data: 'IDKebutuhan',
+                            orderable: false,
+                            searchable: false,
+                            className: 'dt-center',
+                            render: function(data, type, row) {
+                                let baseUrl = `{{ url('kebutuhan') }}`;
+                                return `
                     <div class="btn-group">
                         <button type="button"
                             class="btn btn-icon text-primary rounded-pill dropdown-toggle hide-arrow"
@@ -106,10 +111,59 @@
                         </ul>
                     </div>
                 `;
+                            }
                         }
-                    }
-                ]
-            });
-        </script>
+                    ]
+                });
+            </script>
+        @else
+            <script>
+                $('#table-kebutuhan').DataTable({
+                    responsive: true,
+                    processing: true,
+                    serverSide: true,
+                    ajax: {
+                        url: "{{ route('kebutuhan.index') }}",
+                        error: function(xhr, error, code) {
+                            errorAlert(xhr.statusText);
+                        }
+                    },
+                    columns: [{
+                            data: null,
+                            render: function(data, type, row, meta) {
+                                return meta.settings._iDisplayStart + meta.row + 1;
+                            }
+                        },
+                        {
+                            data: 'posko.user.name'
+                        },
+                        {
+                            data: 'JudulKebutuhan'
+                        },
+                        {
+                            data: 'TanggalKebutuhan'
+                        },
+                        {
+                            data: 'barang.NamaBarang'
+                        },
+                        {
+                            data: 'barang.jenis_barang.JenisBarang'
+                        },
+                        {
+                            data: 'JumlahKebutuhan'
+                        },
+                        {
+                            data: 'JumlahDiterima'
+                        },
+                        {
+                            data: 'user.name'
+                        },
+                        {
+                            data: 'LastUpdateDate'
+                        },
+                    ]
+                });
+            </script>
+        @endif
     @endpush
 @endsection
